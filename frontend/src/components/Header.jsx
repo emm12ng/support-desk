0cb75179e1d76689060dabeba9ea70acedec227b
Emma Nguyen <emm12nguyen@gmail.com>
2022-06-13 18:54:07 -0400

    6/13

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { reset } from '../features/auth/authSlice'
+
+function Header() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
+
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <Link to='/'>Support Desk</Link>
+      </div>
+      <ul>
+            <li>
+              <Link to='/login'>
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to='/register'>
+                <FaUser /> Register
+              </Link>
+            </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
