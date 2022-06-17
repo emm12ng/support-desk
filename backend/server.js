c93385a230eda874b659aef9b2553884902170f0
Emma Nguyen <emm12nguyen@gmail.com>
2022-06-17 16:55:46 -0400

    6/17

--- backend/server.js
+++ backend/server.js
@@ -19,17 +19,20 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-// serve frontend
-if(process.env.NODE_ENV === 'production') {
-    // set build folder as static
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
-} else {
-    app.get('/', (req, res) => {
-        res.status(200).json({message: 'Welcome to the Support Desk API'})
+  
+    // FIX: below code fixes app crashing on refresh in deployment
+    app.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
     })
-}
+  } else {
+    app.get('/', (req, res) => {
+      res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    })
+  }  
 
 app.use(errorHandler)
 
